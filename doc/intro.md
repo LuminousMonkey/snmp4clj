6eeea9381bbf83e8cfcee64007f46f9ee8759030
Mike Aldred <mike.aldred@luminousmonkey.org>
2015-07-09 15:52:07 +0800

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to snmp4clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/snmp4clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns snmp4clj.core-test
+  (:require [clojure.test :refer :all]
+            [snmp4clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
